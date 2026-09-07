610298733e11d46b92a16423fc77060424dde435
Lenin Mendo <leninmendo1414@gmail.com>
2026-09-06 22:37:12 -0500

    Implementacion metodo listar

--- SistemaEstudiante/src/sistema/GestorEstudiantes.java
+++ SistemaEstudiante/src/sistema/GestorEstudiantes.java
@@ -21,6 +21,14 @@ public class GestorEstudiantes {
         estudiantes.add(estudiante);
     }
 
-    
+    public void listar() {
+        if (estudiantes.isEmpty()) {
+            System.out.println("No hay estudiantes registrados.");
+            return;
+        }
+        for (Estudiante e : estudiantes) {
+            e.mostrarDatos();
+        }
+    }
 
 }
