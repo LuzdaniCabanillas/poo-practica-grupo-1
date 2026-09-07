05c2693852dac13df913faa34642e29409f4b188
Lenin Mendo <leninmendo1414@gmail.com>
2026-09-06 21:58:15 -0500

    Agregando clase GestorEstudiante

---
+++ SistemaEstudiante/src/sistema/GestorEstudiantes.java
@@ -0,0 +1,24 @@
+package sistema;
+
+import java.util.ArrayList;
+
+public class GestorEstudiantes {
+
+    private ArrayList<Estudiante> estudiantes;
+
+    public GestorEstudiantes() {
+        estudiantes = new ArrayList<>();
+    }
+
+    public void registrar(Estudiante estudiante) {
+        for (Estudiante e : estudiantes) {
+            if (e.getCodigo().equals(estudiante.getCodigo())) {
+                throw new IllegalArgumentException(
+                        "Estudiante ya existe");
+            }
+
+        }
+        estudiantes.add(estudiante);
+    }
+
+}
