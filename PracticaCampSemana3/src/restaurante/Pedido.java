52212a078a3aa4cf2ba2edb5a2cd062219eb070c
Lenin Mendo <leninmendo1414@gmail.com>
2026-09-20 17:08:43 -0500

    feat (Pedido): Agrega sobrecarga del metodo agregarPlato()

--- PracticaCampSemana3/src/restaurante/Pedido.java
+++ PracticaCampSemana3/src/restaurante/Pedido.java
@@ -5,21 +5,15 @@ import java.time.LocalDateTime;
 public class Pedido {
 
 	private int id;
-
 	private LocalDateTime fechaHora;
-
 	private EstadoPedido estado;
-
 	private double subTotal;
-
 	private Mesa mesa;
-
 	private Cuenta cuenta;
-
 	private DetallePedido[] detallePedido;
 
-	public void agregarPlato() {
-
+	public void agregarPlato(Plato, plato) {
+		System.out.println("Plato agregado: " + plato.getNombre());
 	}
 
 	public double calcularSubTotal() {
