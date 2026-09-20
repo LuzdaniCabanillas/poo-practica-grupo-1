f00fa396ec43573540e9c96392816154cbcefbea
sergiomoriahuanari <sergiogma12@gmail.com>
2026-09-20 17:10:01 -0500

    feat(pedido): prueba de para revertir cambios

--- PracticaCampSemana3/src/restaurante/Pago.java
+++ PracticaCampSemana3/src/restaurante/Pago.java
@@ -13,6 +13,58 @@ public class Pago {
 	private double monto;
 
 	private Cajero cajero;
+        
+            // Constructor
+    public Pago(int id, MetodoPago metodoPago, double monto) {
+
+        this.id = id;
+        this.metodoPago = metodoPago;
+        this.monto = monto;
+        this.fechaHora = LocalDateTime.now();
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public Cajero getCajero() {
+        return cajero;
+    }
+
+    public void setCajero(Cajero cajero) {
+        this.cajero = cajero;
+    }
+    
+    
 
 	public void registrarPago() {
 
